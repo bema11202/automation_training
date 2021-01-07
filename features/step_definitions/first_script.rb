80d1e4b2635b3b4f02603a415943ea7d68ad3644
Bernardo <apphrica@gmail.com>
2021-01-07 11:24:16 -0500

    Merging master_new to master

--- features/step_definitions/first_script.rb
+++ features/step_definitions/first_script.rb
@@ -9,24 +9,25 @@ require 'faker'
 describe 'Puppy adoption' do
   browser = Selenium::WebDriver.for :chrome
   browser.navigate.to 'http://puppies.herokuapp.com'
-  wait = Selenium::WebDriver::Wait.new(timeout: 20)
+  wait = Selenium::WebDriver::Wait.new(timeout: 10)
 
   wait.until { browser.page_source.include?('Home of the Happy Puppy') }
-  wait.until { browser.find_element(:class, 'rounded_button').click }
+  view1 = wait.until { browser.find_element(:class, 'rounded_button') }
+  view1.click
 
-  wait.until { browser.find_element(:class, 'rounded_button').click }
-  wait.until { browser.find_element(:type, 'submit').click }
+  adopt_me = wait.until { browser.find_element(:class, 'rounded_button') }
+  adopt_me.click
+  complete = wait.until { browser.find_element(:class, 'rounded_button') }
+  complete.click
 
-
-  name = browser.find_element(:id, 'order_name')
+  name = wait.until { browser.find_element(:id, 'order_name') }
   name.send_keys(Faker::FunnyName.two_word_name)
   browser.find_element(:id, 'order_address').send_keys(Faker::Address.full_address)
   browser.find_element(:id, 'order_email').send_keys(Faker::Internet.email)
   option = Selenium::WebDriver::Support::Select.new(browser.find_element(:id, 'order_pay_type'))
-  option.select_by(:text, 'Purchase order')
+  # option.select_by(:text, 'Purchase order')
   option.options[rand(1..3)].click
-  # selection = option.options.sample
-  # selection.click
+  sleep 5
   submit_button = browser.find_element(:name, 'commit')
   submit_button.click
   raise('This test has failed!') unless browser.page_source.include? 'Thank you for adopting a puppy!'
